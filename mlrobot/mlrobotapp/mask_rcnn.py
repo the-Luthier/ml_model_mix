e26f3d13daa3b4a679451ab3e9a19a11b51f1d4d
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-25 16:41:56 +0300

    1234

--- mlrobot/mlrobotapp/mask_rcnn.py
+++ mlrobot/mlrobotapp/mask_rcnn.py
@@ -3,16 +3,18 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
+import glob
+from skimage import io
 from mrcnn.config import Config
 from mrcnn import model as modellib
-from mrcnn import utils
+from mrcnn.utils import Dataset
 from mrcnn import visualize
 
 
-class CustomConfig(configparser.ConfigParser):
+class CustomConfig(Config):
     # Customize the necessary configuration parameters
     NAME = "custom_config"
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 10
     # ...
 
     def __init__(self, num_classes):
@@ -24,19 +26,29 @@ class CustomConfig(configparser.ConfigParser):
 
     def load_defaults(self):
         # Set default values
-        self["DEFAULT"] = {
+        self.BACKBONE = "resnet50"
+        self.IMAGE_MIN_DIM = 512
+        self.IMAGE_MAX_DIM = 512
+        # Add other default parameters here
+
+    def save(self, filepath):
+        config = configparser.ConfigParser()
+        config["DEFAULT"] = {
             "NAME": self.NAME,
             "IMAGES_PER_GPU": str(self.IMAGES_PER_GPU),
             "NUM_CLASSES": str(self.NUM_CLASSES),
-            # Add other default parameters here
+            # Add other parameters here
         }
-
-    def save(self, filepath):
-        with open(filepath, "w") as file:
-            self.write(file)
+        with open(filepath, "w") as configfile:
+            config.write(configfile)
 
     def load(self, filepath):
-        self.read(filepath)
+        config = configparser.ConfigParser()
+        config.read(filepath)
+        self.NAME = config["DEFAULT"]["NAME"]
+        self.IMAGES_PER_GPU = int(config["DEFAULT"]["IMAGES_PER_GPU"])
+        self.NUM_CLASSES = int(config["DEFAULT"]["NUM_CLASSES"])
+        # Load other parameters here
 
 
 # Example usage
@@ -49,7 +61,110 @@ loaded_config = CustomConfig(num_classes)
 loaded_config.load("custom_config.ini")
 
 # Access the configuration parameters
-name = loaded_config.get("DEFAULT", "NAME")
-images_per_gpu = loaded_config.getint("DEFAULT", "IMAGES_PER_GPU")
-num_classes = loaded_config.getint("DEFAULT", "NUM_CLASSES")
+name = loaded_config.NAME
+images_per_gpu = loaded_config.IMAGES_PER_GPU
+num_classes = loaded_config.NUM_CLASSES
 # ...
+
+
+# Initialize the Mask R-CNN model
+model = modellib.MaskRCNN(mode="training", config=loaded_config, model_dir="logs")
+
+
+# Create an instance of the Dataset class
+class CustomDataset(Dataset):
+    def load_dataset(self, dataset_dir):
+        # Load and preprocess your dataset
+        # dataset_dir: The directory path where your dataset is located
+
+        # List all image files in the dataset directory
+        image_files = glob.glob(os.path.join(dataset_dir, '*.jpg'))
+
+        # Iterate over the image files
+        for image_file in image_files:
+            # Load the image
+            image = io.imread(image_file)
+
+            # Perform any preprocessing steps (e.g., resizing, normalization)
+
+            # Get the corresponding mask file path based on the image file
+            mask_file = image_file.replace('.jpg', '.png')
+
+            # Load the mask (or bounding box coordinates)
+            mask = io.imread(mask_file)
+
+            # Perform any preprocessing steps on the mask (e.g., convert to binary mask)
+
+            # Get the class ID for the image and mask
+            class_id = int(image_file.split('_')[0])
+
+            # Add the image, mask, and class ID to the dataset attributes
+            self.add_image(
+                source='custom_dataset',
+                image_id=len(self.image_info),
+                path=image_file,
+                mask=mask,
+                class_id=class_id
+            )
+
+    def load_mask(self, image_id):
+        # Replace with your mask generation code
+        pass
+
+    def image_reference(self, image_id):
+        # Implement the logic to return a reference to the image
+        # Replace the placeholder logic with your actual implementation
+        return "Reference to image {}".format(image_id)
+
+    def load_image(self, image_id):
+        # Implement the logic to load the image data for the given image_id
+        # Return the loaded image as a NumPy array
+        pass
+
+
+def split_dataset(dataset, validation_split=0.2):
+    # Split the dataset into training and validation sets
+    dataset_size = len(dataset.image_info)
+    indices = np.arange(dataset_size)
+    np.random.shuffle(indices)
+    split = int(validation_split * dataset_size)
+    train_indices = indices[split:]
+    val_indices = indices[:split]
+
+    train_dataset = CustomDataset()
+    val_dataset = CustomDataset()
+
+    for idx in train_indices:
+        train_dataset.add_image(idx, **dataset.image_info[idx])
+
+    for idx in val_indices:
+        val_dataset.add_image(idx, **dataset.image_info[idx])
+
+    return train_dataset, val_dataset
+
+
+# Create an instance of the CustomDataset class
+dataset = CustomDataset()
+dataset.load_dataset("path/to/dataset")  # Replace with the path to your dataset
+dataset.prepare()
+
+# Split the dataset into training and validation sets
+train_dataset, val_dataset = split_dataset(dataset)
+
+# Train the model
+model.train(train_dataset, val_dataset,
+            learning_rate=config.LEARNING_RATE,
+            epochs=10,
+            layers='all')
+
+# Perform inference on test images
+image_id = 1234
+image = dataset.load_image(image_id)  # Replace `image_id` with the actual image ID
+results = model.detect([image], verbose=1)
+r = results[0]
+
+# Visualize the results
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], dataset.class_names, r['scores'])
+
+
+
