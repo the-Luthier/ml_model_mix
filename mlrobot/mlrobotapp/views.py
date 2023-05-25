3db7063fea1c66ad36970d656867bf8a2141bf95
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-25 17:38:40 +0300

    1234

--- mlrobot/mlrobotapp/views.py
+++ mlrobot/mlrobotapp/views.py
@@ -11,24 +11,87 @@ from dataset import CustomDataset
 from torchvision.transforms import ToTensor
 
 
-@csrf_exempt
-def inference_tfMask_CRNN(request):
-    if request.method == 'POST':
+class Mask_CRNNview:
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request):
         # Load the incoming image
         image = cv2.imdecode(request.FILES['image'].read(), cv2.IMREAD_COLOR)
-        
+
         # Preprocess the image (e.g., resize, normalization)
         # ...
-        
+
         # Perform inference using the trained model
-        num_classes = request(id)
+        num_classes = self.get_num_classes(request)
         model = tfMask_CRNN(num_classes)  # Create an instance of your trained model
         output = model.predict(image)
-        
+
         # Process the output (e.g., post-processing, extracting predictions)
         # ...
-        
+
         # Return the results as a JSON response
         return JsonResponse({'results': output})
-    else:
-        return JsonResponse({'message': 'Invalid request method.'})
+
+    def get_num_classes(self, request):
+        # Extract the number of classes from the request
+        # (e.g., retrieve from the request parameters or headers)
+        num_classes = 0  # Replace with the appropriate code to extract the number of classes
+        return num_classes
+
+
+class SSDResNetView:
+    def __init__(self):
+        self.model = SSDResNetModel(num_classes=10)
+        self.model.build_model()
+
+        self.dataset = CustomDataset()
+        self.dataset.load_dataset("path/to/dataset")
+        self.dataset.prepare()
+
+    def detect_objects(self, request):
+        # Get an image from the dataset
+        image_id = 1234  # Replace with the actual image ID
+        image = self.dataset.load_image(image_id)
+        image_tensor = ToTensor()(image)
+
+        # Perform object detection on the image
+        detection_results = self.detect_objects_with_model(image_tensor)
+
+        # Pass the detection results to the template
+        context = {
+            'image': image,
+            'detection_results': detection_results,
+        }
+        return render(request, 'detect_objects.html', context)
+
+    def detect_objects_with_model(self, image):
+        # Convert the image to a batch of size 1
+        image = image.unsqueeze(0)
+
+        # Make predictions on the image
+        self.model.get_model().eval()
+        with torch.no_grad():
+            predictions = self.model.get_model()(image)
+
+        # Process the predictions and return the results
+        detection_results = self.process_predictions(predictions)
+        return detection_results
+
+    def process_predictions(self, predictions):
+        # Process the model predictions to extract relevant information
+        # such as bounding box coordinates, class labels, and confidence scores
+        # Return the detection results in a suitable format
+        results = []
+
+        # Example code for processing predictions
+        for box, label, score in zip(predictions['boxes'], predictions['labels'], predictions['scores']):
+            result = {
+                'box': box.tolist(),
+                'label': label.item(),
+                'score': score.item(),
+            }
+            results.append(result)
+
+        return results
